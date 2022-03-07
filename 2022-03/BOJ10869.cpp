54a0ec01f437aa592ad71d02a1171c292bbbae78
lzmgl <lzmgl5874@gmail.com>
2022-03-07 21:38:41 +0900

    20220307_02

---
+++ 2022-03/BOJ10869.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+main() {
+	int a = 0,b = 0;
+    cin >> a >> b ;
+    printf("%d\n", a+b);
+    printf("%d\n", a-b);
+    printf("%d\n", a*b);
+    printf("%d\n", a/b);
+    printf("%d\n", a%b);
+}
