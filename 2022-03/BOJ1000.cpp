f905c822cf8726c96aecb2b9acb5595599f4c8be
lzmgl <lzmgl5874@gmail.com>
2022-03-07 16:42:24 +0900

    20220307_01

---
+++ 2022-03/BOJ1000.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+main() {
+	int a = 0,b = 0;
+    cin >> a >> b ;
+	int c = 0;
+	c = a + b;
+    cout << c;
+}
---
+++ 2022-03/BOJ1008.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+main() {
+	double a = 0,b = 0;
+    cin >> a >> b ;
+	double c = 0;
+	c = a / b;
+    printf("%.9f", c);
+}
