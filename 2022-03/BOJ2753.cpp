cab20ee0977573bf54095dbb6c3cd2a71abd0215
lzmgl <lzmgl5874@gmail.com>
2022-03-11 21:56:40 +0900

    20220311_05

---
+++ 2022-03/BOJ2753.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+main() {
+    int a=0;
+    cin >> a;
+    if ((a%4==0) && (a%100!=0 || a%400 ==0)){
+        cout << "1";
+    } else {
+        cout << "0" ;
+    }
+}
