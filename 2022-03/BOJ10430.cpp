5c2c324586cbb31d06ca8d0f2cf1b88d4ae15e3e
lzmgl <lzmgl5874@gmail.com>
2022-03-11 21:19:59 +0900

    20220311_02

---
+++ 2022-03/BOJ10430.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+main() {
+	int a = 0, b = 0, c = 0;
+    cin >> a >> b >> c;
+    cout << (a+b)%c << endl;
+    cout << ((a%c)+(b%c))%c << endl;
+    cout << (a*b)%c << endl;
+    cout << ((a%c)*(b%c))%c ;
+}
