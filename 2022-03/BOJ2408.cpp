d06204e088ed3d79ca6a49b07bc30c80fae83e66
Jehyeon An <jehyeonan@Jehyeonui-MacBookPro.local>
2022-03-13 13:15:28 +0900

    20220313_01

---
+++ 2022-03/BOJ2408.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+main() {
+	int a = 0, b = 0, c = 0;
+    cin >> a >> b >> c;
+    if (a==b && b==c){
+        a=10000+a*1000;
+        cout << a;
+    } else if (a==b || b==c || a==c){
+        a=1000+a*100;
+        cout<<a;
+    } else {
+        if (a>b){
+            if (b<c){
+                if (a>c){
+                    a*=100;
+                    cout << a;
+                } else {
+                    c*=100;
+                    cout << c;
+                }
+            } else if (b>c){
+                a*=100;
+                cout << a;
+            }
+            
+        } else if (a>c)
+    }
+}
