e2696513f0d09940db769fa4e07c1474c263112a
lzmgl <lzmgl5874@gmail.com>
2022-03-20 00:11:06 +0900

    20220320_01

--- 2022-03/BOJ10699.py
+++ 2022-03/BOJ10699.py
@@ -1,7 +1,4 @@
 import datetime
 d=datetime.datetime.utcnow() + datetime.timedelta(hours=9)
-y=d.year
-m=d.month
-dd=d.day
-m='{:02d}'.format(m)
-print(f"{y}-{m}-{dd}")
+m='{:02d}'.format(d.month)
+print(f"{d.year}-{m}-{d.day}")
