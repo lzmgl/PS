d41e37b223191b20d75a7d4b318a469332c340c9
lzmgl <lzmgl5874@gmail.com>
2022-03-23 21:20:17 +0900

    20220323_03

---
+++ 2022-03/BOJ2480.py
@@ -0,0 +1,23 @@
+'''
+import sys
+import math
+A, B, C = map(int, sys.stdin.readline().split())
+if A==B==C:
+    print(10000+A*1000)
+elif A!=B and A!=C and B!=C:
+    print(max(A,B,C)*100)
+else :
+    if A==B:
+        print(1000+A*100)
+    elif A==C:
+        print(1000+A*100)
+    else:
+        print(1000+B*100)
+'''
+*_,a,b,c=sorted(input())
+print(a,b,c)
+print(['1'+b,c])
+print([a<b<c])
+print('000'[a<c:])
+print(['1'+b,c][a<b<c]+'000'[a<c:])
+print([a,b][False])
