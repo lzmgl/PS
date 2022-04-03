a5901ef918b99f8e02f09122220b388843a01493
lzmgl <lzmgl5874@gmail.com>
2022-04-03 15:46:46 +0900

    20220403_05

--- 2022-04/BOJ3009.py
+++ 2022-04/BOJ3009.py
@@ -1,3 +1,9 @@
 import sys
+arr1 = [0]*1001
+arr2 = [0]*1001
 for i in range(3):
     a1, b1 = map(int,sys.stdin.readline().split())
+    arr1[a1]+=1
+    arr2[b1]+=1
+print(arr1[arr1>1])
+print(arr2[arr2>1])
