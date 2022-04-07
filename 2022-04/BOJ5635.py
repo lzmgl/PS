23c0405598b3eab504e11fc41282ea6c884b6559
lzmgl <lzmgl5874@gmail.com>
2022-04-07 23:20:26 +0900

    20220407_02

--- 2022-04/BOJ5635.py
+++ 2022-04/BOJ5635.py
@@ -7,10 +7,16 @@ for _ in range(N):
     day=day.zfill(2)
     year=year.zfill(4)
     birth=int(year+month+day)
-    arr.append(list(name, birth))
+    tmp = [name, birth]
+    arr.append(tmp)
 maxN=0
+minN=99999999
 for i in range(N):
-    maxN=max(maxN, arr[0][i])
+    maxN=max(maxN, arr[i][1])
+    minN=min(minN, arr[i][1])
     if arr[i][1]==maxN:
         res=arr[i][0]
+    if arr[i][1]==minN:
+        res2=arr[i][0]
 print(res)
+print(res2)
