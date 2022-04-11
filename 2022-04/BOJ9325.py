f28e07f8d793d8ce9fc871c5381a13b3f7dda7cd
lzmgl <lzmgl5874@gmail.com>
2022-04-11 18:47:27 +0900

    20220411_04

---
+++ 2022-04/BOJ9325.py
@@ -0,0 +1,10 @@
+T=int(input())
+for _ in range(T):
+    s=int(input())
+    n=int(input())
+    for _ in range(n):
+        q, p = map(int, input().split())
+        q*=p
+        s+=q
+    print(s)
+    
