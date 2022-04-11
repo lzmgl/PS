51837e768aeb9b3c333436b2d9dbe0a7dc2d0043
lzmgl <lzmgl5874@gmail.com>
2022-04-12 00:25:19 +0900

    20220412_06

---
+++ 2022-04/BOJ2490.py
@@ -0,0 +1,7 @@
+# 2490번
+# https://www.acmicpc.net/problem/2490
+import sys
+dic={0:'D', 4:'E', 3:'A', 2:'B', 1:'C'}
+for _ in range(3):
+    tmp=list(map(int,sys.stdin.readline().split()))
+    print(dic[sum(tmp)])
