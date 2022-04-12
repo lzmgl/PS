6cc51d71f7bf44c247ccd83cce522c4ada915831
lzmgl <lzmgl5874@gmail.com>
2022-04-12 23:03:56 +0900

    20220412_19

---
+++ 2022-04/BOJ5543.py
@@ -0,0 +1,10 @@
+# 5543번
+# https://www.acmicpc.net/problem/5543
+import sys
+burger=[]
+coke=[]
+for _ in range(3):
+    burger.append(int(sys.stdin.readline()))
+for _ in range(2):
+    coke.append(int(sys.stdin.readline()))
+print(min(burger)+min(coke)-50)
