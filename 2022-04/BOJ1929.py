b8d52350968f842b367851ac0101fdbe5bff8406
lzmgl <lzmgl5874@gmail.com>
2022-04-13 22:13:31 +0900

    20220413_09

---
+++ 2022-04/BOJ1929.py
@@ -0,0 +1,14 @@
+# 1929번 소수 구하기
+# https://www.acmicpc.net/problem/1929
+import sys
+M, N= map(int, sys.stdin.readline().split())
+is_primes = [True for i in range(1000001)] 
+for i in range(2, N+1): 
+    if is_primes[i]: 
+        for j in range(i * i, N+1, i): 
+            is_primes[j] = False
+is_primes[0] = is_primes[1] = False
+
+for i in range(M, N+1):
+    if is_primes[i]:
+        print(i)
