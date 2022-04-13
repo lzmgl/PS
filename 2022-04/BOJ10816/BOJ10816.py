291e40b3068f65d742d4637d916cb09993fcdd0c
lzmgl <lzmgl5874@gmail.com>
2022-04-13 23:41:32 +0900

    20220413_14

---
+++ 2022-04/BOJ10816/BOJ10816.py
@@ -0,0 +1,7 @@
+# 10816번 숫자 카드 2
+# https://www.acmicpc.net/problem/10816
+import sys, os
+    
+sys.stdin = open('{}/BOJ10816.txt'.format(os.path.dirname(os.path.realpath(__file__))))
+N=int(sys.stdin.readline())
+dataN=list(map(int, sys.stdin.readline().split()))
--- 2022-04/BOJ2751.py
+++ 2022-04/BOJ2751.py
@@ -1,4 +1,9 @@
-# 2751번
+# 2751번 수 정렬하기2
 # https://www.acmicpc.net/problem/2751
 import sys
 N=int(sys.stdin.readline())
+data=[]
+for _ in range(N):
+    data.append(int(sys.stdin.readline()))
+data.sort()
+print(*data,sep='\n')
