1a971af06fd6dcd30cd44812d76e19cd7b7fa85f
lzmgl <lzmgl5874@gmail.com>
2022-04-14 03:00:02 +0900

    20220414_06

--- 2022-04/BOJ11050/BOJ11050.py
+++ 2022-04/BOJ11050/BOJ11050.py
@@ -3,4 +3,26 @@
 import sys, os
     
 sys.stdin = open('{}/BOJ11050.txt'.format(os.path.dirname(os.path.realpath(__file__))))
-N=int(sys.stdin.readline())
+A, B=map(int, sys.stdin.readline().split())
+def bino_coef(n, k):
+    if k > n:
+        return 0
+
+    # 1.
+    cache = [[-1 for _ in range(n+1)] for _ in range(n+1)]
+    # 2.
+    def choose(times, got):
+        # 3.
+        if times == n:
+            return got == k
+        # 4.
+        if cache[times][got] != -1:
+            return cache[times][got]
+
+	# 5.
+        cache[times][got] = choose(times+1, got) + choose(times+1, got+1)
+        return cache[times][got]
+
+    # 6.
+    return choose(0, 0)
+print(bino_coef(A,B))
