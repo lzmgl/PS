a66197364afd95e9bf5b9d0eea4423a849837269
lzmgl <lzmgl5874@gmail.com>
2022-04-13 17:28:12 +0900

    20220413_02

---
+++ 2022-04/BOJ11654.py
@@ -0,0 +1,6 @@
+# 11654번
+# https://www.acmicpc.net/problem/11654
+import sys
+N=(sys.stdin.readline().rstrip())
+print(ord(N))
+#print(ord(input()))
---
+++ 2022-04/BOJ11720.py
@@ -0,0 +1,4 @@
+# 11720번
+# https://www.acmicpc.net/problem/11720
+import sys
+N=int(sys.stdin.readline())
