735b5ad0a9291c89d732b3bdfb0ea6d5fd4d1986
lzmgl <lzmgl5874@gmail.com>
2022-04-14 20:47:36 +0900

    20220414_10

---
+++ 2022-04/BOJ1874/BOJ1874.py
@@ -0,0 +1,73 @@
+# 1874번 스택 수열
+# https://www.acmicpc.net/problem/1874
+import sys, os
+    
+sys.stdin = open('{}/BOJ1874.txt'.format(os.path.dirname(os.path.realpath(__file__))))
+N=int(sys.stdin.readline())
+st = [int(sys.stdin.readline()) for _ in range(N)]
+'''
+height=0
+maxH=0
+for i in range(N):
+    height=compStack[i]
+    if maxH<=height:
+        for _ in range(height-maxH):
+            print('+')
+        print('-')
+        maxH=max(compStack[i], maxH)
+    if height<maxH:
+        print('-')
+'''
+data=[-2]
+res=[]
+idx=0
+i=1
+print(st)
+while idx<N:
+    if data[-1]!=st[idx]:
+        res.append('+')
+        data.append(i)
+        i+=1
+    else:
+        res.append('-')
+        data.pop()
+        idx+=1
+        if idx>=N:
+            break
+    if data[-1]>st[idx]:
+        print('NO')
+        exit()
+for item in res:
+    print(item)
+
+
+
+
+'''
+data=[]
+print(compStack)
+idx=0
+stackidx=0
+res=[]
+maxN=1
+i=1
+while i<N+1:
+    print(f'compStack={compStack[idx]}')
+    if i<=compStack[idx]:
+        print(f'push {i}')
+        maxN=max(maxN, i)
+        data.append(i)
+        res.append('+')
+    else:
+        
+    if data[-1]==compStack[idx]:
+        print(f'pop {compStack[idx]}')
+        data.pop()
+        res.append('-')
+        idx+=1
+        i-=1
+        print(f'i={i}')
+    i+=1
+print(data)
+print(res)
+'''
