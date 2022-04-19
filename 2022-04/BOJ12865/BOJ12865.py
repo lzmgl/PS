a57992ee6697791ce01b95534a9a64c4eb98bd18
lzmgl <lzmgl5874@gmail.com>
2022-04-19 22:11:42 +0900

    20220419_03

--- 2022-04/BOJ12865/BOJ12865.py
+++ 2022-04/BOJ12865/BOJ12865.py
@@ -3,8 +3,27 @@
 import sys, os
     
 sys.stdin = open('{}/BOJ12865.txt'.format(os.path.dirname(os.path.realpath(__file__))))
-N, K=map(int,sys.stdin.readline().split())
+N, capacity=map(int,sys.stdin.readline().split())
+
 data=[]
 for _ in range(N):
     data.append(list(map(int,sys.stdin.readline().split())))
-print(data)
+    
+
+DP = [[-1 for i in range(capacity+1)] for _ in range(N)]
+
+def knapsack(idx, cap):
+    if idx==N:
+        return 0
+    ret=DP[idx][cap]
+    if ret != -1:
+        return ret
+    if data[idx][0]<=cap:
+        ret=knapsack(idx+1, cap-data[idx][0])+data[idx][1]
+    ret = max(ret, knapsack(idx+1, cap))
+    DP[idx][cap]=ret
+    return ret
+
+print(knapsack(0,capacity))
+
+
