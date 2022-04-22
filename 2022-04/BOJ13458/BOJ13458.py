ef3383a94c4add0ebbac85d28425d3ed625e7236
lzmgl <lzmgl5874@gmail.com>
2022-04-22 23:41:29 +0900

    20220422_04

--- 2022-04/BOJ13458/BOJ13458.py
+++ 2022-04/BOJ13458/BOJ13458.py
@@ -4,3 +4,14 @@ import sys, os
     
 sys.stdin = open('{}/BOJ13458.txt'.format(os.path.dirname(os.path.realpath(__file__))))
 N=int(sys.stdin.readline())
+A_list=list(map(int,sys.stdin.readline().split()))
+B, C=map(int,sys.stdin.readline().split())
+cnt=0
+for room in A_list:
+    cnt+=1
+    if room > B:
+        room-=B
+        cnt += room//C
+        if room%C != 0:
+            cnt +=1
+print(cnt)
