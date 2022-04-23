a228c6e8d3ac7fdd8e073ff32134b32ee6d97ecf
lzmgl <lzmgl5874@gmail.com>
2022-04-23 23:22:56 +0900

    20220423_02

--- 2022-04/BOJ3058/BOJ3058.py
+++ 2022-04/BOJ3058/BOJ3058.py
@@ -1,7 +1,6 @@
 # 3058번 짝수를 찾아라
 # https://www.acmicpc.net/problem/3058
 import sys, os
-    
 sys.stdin = open('{}/BOJ3058.txt'.format(os.path.dirname(os.path.realpath(__file__))))
 N=int(sys.stdin.readline())
 for _ in range(N):
