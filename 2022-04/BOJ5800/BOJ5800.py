a2c8ac1b26f653ba7e48af7bd2df97de311ee919
lzmgl <lzmgl5874@gmail.com>
2022-04-24 18:41:44 +0900

    20220424_16

--- 2022-04/BOJ5800/BOJ5800.py
+++ 2022-04/BOJ5800/BOJ5800.py
@@ -3,3 +3,13 @@
 import sys, os
 sys.stdin = open('{}/BOJ5800.txt'.format(os.path.dirname(os.path.realpath(__file__))))
 N=int(sys.stdin.readline())
+for i in range(1, N+1):
+    tmp=list(map(int, sys.stdin.readline().split()))
+    tmp=tmp[1:]
+    tmp.sort()
+    maxN, minN=max(tmp), min(tmp)
+    gap=0
+    for idx in range(len(tmp)-1):
+        gap=max(tmp[idx+1]-tmp[idx], gap)
+    print(f'Class {i}')
+    print(f'Max {maxN}, Min {minN}, Largest gap {gap}')
