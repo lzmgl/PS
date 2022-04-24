16e48a5df6e0102c519532773537a92a8a6d9cf2
lzmgl <lzmgl5874@gmail.com>
2022-04-24 12:26:43 +0900

    20220424_01

---
+++ 2022-04/BOJ1676/BOJ1676.py
@@ -0,0 +1,15 @@
+# 1676번 팩토리얼 0의 개수
+# https://www.acmicpc.net/problem/1676
+import sys, os
+from math import factorial
+    
+sys.stdin = open('{}/BOJ1676.txt'.format(os.path.dirname(os.path.realpath(__file__))))
+N=int(sys.stdin.readline())
+C=str(factorial(N))[::-1]
+cnt=0
+for c in C:
+    if c == '0':
+        cnt+=1
+    else:
+        break
+print(cnt)
