86ad43bff6d57f8a92480fda3df884f5bd7e9ff0
lzmgl <lzmgl5874@gmail.com>
2022-04-30 00:16:48 +0900

    20220430_3

---
+++ 2022-04/BOJ4949/BOJ4949.py
@@ -0,0 +1,39 @@
+# 4949번 균형잡힌 세상
+# https://www.acmicpc.net/problem/4949
+import sys, os
+sys.stdin = open('{}/BOJ4949.txt'.format(os.path.dirname(os.path.realpath(__file__))))
+s=sys.stdin.readline()
+while s != '.' or s!='.\n':
+    # print(s)
+    stack=[]
+    bal=True
+    try:
+        for c in s:
+            if c=='(':
+                stack.append(c)
+            elif c==')':
+                tmp=stack.pop()
+                if tmp!='(':
+                    bal=False
+                    print('no')
+                    break
+            if c=='[':
+                stack.append(c)
+            elif c==']':
+                tmp=stack.pop()
+                if tmp!='[':
+                    bal=False
+                    print('no')
+                    break
+    except:
+        bal=False
+        print('no')
+    # print(len(stack))
+    if bal:
+        if len(stack) ==0:
+            print('yes')
+        else:
+            print('no')
+    s=sys.stdin.readline()
+    if s == '.' or s=='.\n':
+        break
