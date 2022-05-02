9356da4ade9200c18406eee5ab69f925e3bf752b
lzmgl <lzmgl5874@gmail.com>
2022-05-03 00:17:56 +0900

    20220503_3

---
+++ 2022-05/BOJ18111/BOJ18111.py
@@ -0,0 +1,5 @@
+# 18111번 마인크래프트
+# https://www.acmicpc.net/problem/18111
+import sys, os
+sys.stdin = open('{}/BOJ18111.txt'.format(os.path.dirname(os.path.realpath(__file__))))
+N=int(sys.stdin.readline())
