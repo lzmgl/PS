f9083737eb02b91c1c69d23bca6f8926196b0796
lzmgl <lzmgl5874@gmail.com>
2022-05-08 01:35:11 +0900

    20220508_0

---
+++ 2022-05/BOJ25083/BOJ25083.py
@@ -0,0 +1,11 @@
+# 25083번 새싹
+# https://www.acmicpc.net/problem/25083
+import sys, os
+sys.stdin = open('{}/BOJ25083.txt'.format(os.path.dirname(os.path.realpath(__file__))))
+s='''         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |'''
+print(s)
