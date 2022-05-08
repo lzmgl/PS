33e17dc922d57282bdd2a52ae7bf0ee7a7e9c0ea
lzmgl <lzmgl5874@gmail.com>
2022-05-08 16:43:59 +0900

    20220508_1

---
+++ 2022-05/BOJ5430/BOJ5430.py
@@ -0,0 +1,5 @@
+# 5430번 AC
+# https://www.acmicpc.net/problem/5430
+import sys, os
+sys.stdin = open('{}/BOJ5430.txt'.format(os.path.dirname(os.path.realpath(__file__))))
+N=int(sys.stdin.readline())
