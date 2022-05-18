f7febb7687512c06c1974cf3895715e035169a4e
lzmgl <lzmgl5874@gmail.com>
2022-05-18 22:30:49 +0900

    20220518_0

--- 2022-05/BOJ1193/BOJ1193.py
+++ 2022-05/BOJ1193/BOJ1193.py
@@ -1,5 +1,28 @@
-# 1193¹ø ºÐ¼öÃ£±â
+# 1193ï¿½ï¿½ ï¿½Ð¼ï¿½Ã£ï¿½ï¿½
 # https://www.acmicpc.net/problem/1193
+from re import I
 import sys, os
 sys.stdin = open('{}/BOJ1193.txt'.format(os.path.dirname(os.path.realpath(__file__))))
 N=int(sys.stdin.readline())
+i=0
+cnt=0
+while N>cnt:
+    i+=1
+    cnt+=i
+cnt-=i
+if i%2!=0:
+    y=i+1
+    for x in range(1, i+1):
+        cnt+=1
+        y-=1
+        if cnt==N:
+            print(f'{y}/{x}')
+            exit()
+else:
+    x=i+1
+    for y in range(1, i+1):
+        cnt+=1
+        x-=1
+        if cnt==N:
+            print(f'{y}/{x}')
+            exit()
