f1910b3ae8f2c08de999031c42a2ce5aaa90156b
lzmgl <lzmgl5874@gmail.com>
2022-05-18 22:31:32 +0900

    20220518_1

--- 2022-05/BOJ1193/BOJ1193.py
+++ 2022-05/BOJ1193/BOJ1193.py
@@ -1,6 +1,5 @@
-# 1193�� �м�ã��
+# 1193번 분수찾기
 # https://www.acmicpc.net/problem/1193
-from re import I
 import sys, os
 sys.stdin = open('{}/BOJ1193.txt'.format(os.path.dirname(os.path.realpath(__file__))))
 N=int(sys.stdin.readline())
