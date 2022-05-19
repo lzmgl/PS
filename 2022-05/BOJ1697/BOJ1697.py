1aee04e8cf04fb024dbbe90e0b2d3446495d5a38
lzmgl <lzmgl5874@gmail.com>
2022-05-19 22:29:53 +0900

    20220519_1

--- 2022-05/BOJ1697/BOJ1697.py
+++ 2022-05/BOJ1697/BOJ1697.py
@@ -2,4 +2,20 @@
 # https://www.acmicpc.net/problem/1697
 import sys, os
 sys.stdin = open('{}/BOJ1697.txt'.format(os.path.dirname(os.path.realpath(__file__))))
-N=int(sys.stdin.readline())
+from collections import deque
+N, K=map(int,sys.stdin.readline().split())
+board = [0 for _ in range(200000)]
+
+q=deque([N])
+
+while q:
+    x=q.popleft()
+    if x==K:
+        break
+    dx=[x-1, x+1, 2*x]
+    for i in dx:
+        if 0<=i<200000 and not board[i]:
+            board[i]=board[x]+1
+            q.append(i)
+            
+print(board[K])
