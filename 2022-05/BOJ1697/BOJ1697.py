abbf20f545939e671d2ad62017e0fdfe37f29f86
lzmgl <lzmgl5874@gmail.com>
2022-05-19 22:22:09 +0900

    20220519_0

---
+++ 2022-05/BOJ1697/BOJ1697.py
@@ -0,0 +1,5 @@
+# 1697번 숨바꼭질
+# https://www.acmicpc.net/problem/1697
+import sys, os
+sys.stdin = open('{}/BOJ1697.txt'.format(os.path.dirname(os.path.realpath(__file__))))
+N=int(sys.stdin.readline())
