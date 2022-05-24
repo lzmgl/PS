4bdbed972a52fc48cebc1f1c70490922049bb319
lzmgl <lzmgl5874@gmail.com>
2022-05-24 20:49:00 +0900

    20220524_0

--- 2022-04/BOJ24417/BOJ24417.py
+++ 2022-04/BOJ24417/BOJ24417.py
@@ -6,35 +6,40 @@ sys.stdin = open('{}/BOJ24417.txt'.format(os.path.dirname(os.path.realpath(__fil
 N=int(sys.stdin.readline())
 mod=1000000007
 
-f=[0]*(N+1)
-cnt2=0
-def fibonacci(n) :
-    global cnt2
-    f[1]=f[2]=1
-    for i in range(3,n+1):
-        cnt2+=1
-        cnt2%=mod
-        f[i] = f[i-1] + f[i-2];  # 코드2
-    return f[n];
-def matMul(a, b):
-    global mod
-    n=len(a)
-    data=[[0,0],[0,0]]
-    for i in range(n):
-        for j in range(n):
-            for k in range(n):
-                data[i][j]= data[i][j]+(a[i][k]*b[k][j])
-            data[i][j]%=mod
-    return data
+# f=[0]*(N+1)
+# cnt2=0
+# def fibonacci(n) :
+#     global cnt2
+#     f[1]=f[2]=1
+#     for i in range(3,n+1):
+#         cnt2+=1
+#         cnt2%=mod
+#         f[i] = f[i-1] + f[i-2];  # 코드2
+#     return f[n];
+# def matMul(a, b):
+#     global mod
+#     n=len(a)
+#     data=[[0,0],[0,0]]
+#     for i in range(n):
+#         for j in range(n):
+#             for k in range(n):
+#                 data[i][j]= data[i][j]+(a[i][k]*b[k][j])
+#             data[i][j]%=mod
+#     return data
             
-matAns=[[1,0], [0,1]]
-matFibo=[[1,1], [1,0]]
+# matAns=[[1,0], [0,1]]
+# matFibo=[[1,1], [1,0]]
 
-fibonacci(N)
-while N>0:
-    if N%2==1:
-        matAns=matMul(matAns,matFibo)
-    matFibo=matMul(matFibo,matFibo)
-    N//=2
-
-print(matAns[0][1], cnt2)
+# fibonacci(N)
+# while N>0:
+#     if N%2==1:
+#         matAns=matMul(matAns,matFibo)
+#     matFibo=matMul(matFibo,matFibo)
+#     N//=2
+a=b=1
+for i in range(2, N):
+    tmp=a+b
+    if tmp>=mod:
+        tmp-=mod
+    a,b=b,tmp
+print(b, N-2)
