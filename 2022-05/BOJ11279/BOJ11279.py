92d1d2a65bd480ddacacea72ad9283410caceb27
lzmgl <lzmgl5874@gmail.com>
2022-05-25 16:09:48 +0900

    20220525_5

--- 2022-05/BOJ11279/BOJ11279.py
+++ 2022-05/BOJ11279/BOJ11279.py
@@ -1,5 +1,5 @@
 # 11279번 최대 힙
-# hepaq, 최대힙
+# hepaq, 최대힙, 최대힙트릭
 # https://www.acmicpc.net/problem/11279
 import sys, os
 sys.stdin = open('{}/BOJ11279.txt'.format(os.path.dirname(os.path.realpath(__file__))))
@@ -8,7 +8,7 @@ N=int(sys.stdin.readline())
 heap=[]
 for _ in range(N):
     x = int(sys.stdin.readline())
-    if x>0:
+    if x!=0:
         heapq.heappush(heap, (-x, x)) #최대힙 트릭
     else:
         try:
