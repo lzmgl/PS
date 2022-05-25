5b69f0383f4f6314837268b521b18b6679957c5b
lzmgl <lzmgl5874@gmail.com>
2022-05-25 16:06:56 +0900

    20220525_3

--- 2022-05/BOJ1927/BOJ1927.py
+++ 2022-05/BOJ1927/BOJ1927.py
@@ -2,4 +2,15 @@
 # https://www.acmicpc.net/problem/1927
 import sys, os
 sys.stdin = open('{}/BOJ1927.txt'.format(os.path.dirname(os.path.realpath(__file__))))
+import heapq
 N=int(sys.stdin.readline())
+heap=[]
+for _ in range(N):
+    x = int(sys.stdin.readline())
+    if x>0:
+        heapq.heappush(heap, x)
+    else:
+        try:
+            print(heapq.heappop(heap))
+        except:
+            print(0)
