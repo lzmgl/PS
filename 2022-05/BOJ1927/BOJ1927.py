9437b3b664c3f7190e976588f1443e56dcc12139
lzmgl <lzmgl5874@gmail.com>
2022-05-25 16:03:25 +0900

    20220525_2

---
+++ 2022-05/BOJ1927/BOJ1927.py
@@ -0,0 +1,5 @@
+# 1927번 최소 힙
+# https://www.acmicpc.net/problem/1927
+import sys, os
+sys.stdin = open('{}/BOJ1927.txt'.format(os.path.dirname(os.path.realpath(__file__))))
+N=int(sys.stdin.readline())
