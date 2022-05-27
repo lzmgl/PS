b722efbaac310da193f58a8003530a2e67750c6b
lzmgl <lzmgl5874@gmail.com>
2022-05-27 22:06:22 +0900

    20220527_1

---
+++ 2022-05/BOJ1992/BOJ1992.py
@@ -0,0 +1,5 @@
+# 1992번 쿼드트리
+# https://www.acmicpc.net/problem/1992
+import sys, os
+sys.stdin = open('{}/BOJ1992.txt'.format(os.path.dirname(os.path.realpath(__file__))))
+N=int(sys.stdin.readline())
