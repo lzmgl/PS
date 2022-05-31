c273fb0ae284ae24bba9dfbe75b6ba4f93957e42
lzmgl <lzmgl5874@gmail.com>
2022-06-01 01:54:39 +0900

    20220601_0

--- num_words.py
+++ num_words.py
@@ -1,10 +1,22 @@
 import os, sys
-sys.stdin = open('b.txt'.format(os.path.dirname(os.path.realpath(__file__))))
+sys.stdin = open('tmp.txt'.format(os.path.dirname(os.path.realpath(__file__))))
+sys.stdout = open('output.txt','w')
+w=open('tmp2.txt', 'w', encoding='utf-8')
 words = sys.stdin.readlines()
 cnt=0
 for sentence in words:
-    for c in sentence:
-        if c != ' ' and c != '\n':
-            cnt+=1
+    if sentence == '\n' or sentence == ' \n' or sentence == '' or sentence == ' ':
+        continue
+    line = sentence.strip()
+    if len(sentence) < 10:
+        print(sentence)
+        line = line.replace('\n', ' ')
+        w.write(line)
+    else:
+        w.write(line)
+        w.write('\n')
+    # for c in sentence:
+    #     if c != ' ' and c != '\n':
+    #         cnt+=1
 
-print(cnt-57)
+# print(cnt-57)
