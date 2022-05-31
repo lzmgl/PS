b6fd05908bf096a6d782cc3c0d7e419dd4ecc3e8
lzmgl <lzmgl5874@gmail.com>
2022-05-31 20:46:11 +0900

    20220531_1

---
+++ 2022-05/BOJ1149/BOJ1149.py
@@ -0,0 +1,18 @@
+# 1149번 RGB거리
+# https://www.acmicpc.net/problem/1149
+import sys, os
+sys.stdin = open('{}/BOJ1149.txt'.format(os.path.dirname(os.path.realpath(__file__))))
+sys.setrecursionlimit(100001)
+N=int(sys.stdin.readline())
+rgb = [[int(x) for x in sys.stdin.readline().split()] for i in range(N)]
+
+def dp(lev):
+    if lev == -1:
+        return [0,0,0]
+    cache = dp(lev-1)
+    rgb[lev][0] += min(cache[1], cache[2])
+    rgb[lev][1] += min(cache[0], cache[2])
+    rgb[lev][2] += min(cache[0], cache[1])
+    return rgb[lev]
+
+print(min(dp(N-1)))
