c415b3d2c52f2f1ce23583319c32e513959b17cd
lzmgl <lzmgl5874@gmail.com>
2022-06-08 21:03:58 +0900

    20220608_2

--- 2022-06/BOJ2193/BOJ2193.py
+++ 2022-06/BOJ2193/BOJ2193.py
@@ -4,9 +4,11 @@ import sys, os
 sys.stdin = open('{}/BOJ2193.txt'.format(os.path.dirname(os.path.realpath(__file__))))
 N=int(sys.stdin.readline())
 dp=[0]*(N+1)
-dp[1]=len([1])
-dp[2]=len([10])
-dp[3]=len([100, 101])
-dp[4]=len([1000, 1010, 1001])
-dp[5]=len([10000, 10100, 10101, 10010])
-dp[6]=len([100000, 101000, 100100, 100010, 100001, 101010, 101001, 100101])
+dp[1]=1
+if N>1:
+    dp[2]=1
+if N>2:
+    dp[3]=2
+for i in range(2, N+1):
+    dp[i]=dp[i-1]+dp[i-2]
+print(dp[N])
