302354a55fa2bcff179ca3b262d545129c75469e
lzmgl <lzmgl5874@gmail.com>
2022-06-08 20:17:42 +0900

    20220608_0

---
+++ 2022-06/BOJ2193/BOJ2193.py
@@ -0,0 +1,5 @@
+# 2193번 이친수
+# https://www.acmicpc.net/problem/2193
+import sys, os
+sys.stdin = open('{}/BOJ2193.txt'.format(os.path.dirname(os.path.realpath(__file__))))
+N=int(sys.stdin.readline())
--- 2022-06/BOJ9251/BOJ9251.py
+++ 2022-06/BOJ9251/BOJ9251.py
@@ -5,12 +5,12 @@ import sys, os
 sys.stdin = open('{}/BOJ9251.txt'.format(os.path.dirname(os.path.realpath(__file__))))
 s1=sys.stdin.readline().strip()
 s2=sys.stdin.readline().strip()
-maxlen=max(len(s1), len(s2))
+maxlen=max(len(s1), len(s2))+1
 lcs=[[0]*maxlen for _ in range(maxlen)]
-for i in range(1, len(s1)):
-    for j in range(1, len(s2)):
-        if s1[i]==s2[j]:
+for i in range(1, len(s1)+1):
+    for j in range(1, len(s2)+1):
+        if s1[i-1]==s2[j-1]:
             lcs[i][j]=lcs[i-1][j-1]+1
         else:
             lcs[i][j]=max(lcs[i-1][j], lcs[i][j-1])
-print(max(map(max, lcs))+1)
+print(max(map(max, lcs)))
