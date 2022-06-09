db1ebb1ea229262ba708843b0b9035014895ffe4
lzmgl <lzmgl5874@gmail.com>
2022-06-10 02:16:01 +0900

    20220610_2

---
+++ 2022-06/BOJ14501/BOJ14501.py
@@ -0,0 +1,13 @@
+# 14501번 퇴사
+# https://www.acmicpc.net/problem/14501
+import sys, os
+sys.stdin = open('{}/BOJ14501.txt'.format(os.path.dirname(os.path.realpath(__file__))))
+N=int(sys.stdin.readline())
+data = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+dp=[0]*(N+1)
+for i in range(N-1, -1, -1):
+    if i+data[i][0] > N:
+        dp[i]=dp[i+1]
+    else:
+        dp[i]=max(dp[i+1], data[i][1]+dp[i+data[i][0]])
+print(dp[0])
