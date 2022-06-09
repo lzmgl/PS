c037a59ed52e9ca1ebfdeb264fd7ec8a56fe5d7c
lzmgl <lzmgl5874@gmail.com>
2022-06-09 10:24:00 +0900

    20220609_0

---
+++ 2022-06/BOJ14501/BOJ14501.py
@@ -0,0 +1,5 @@
+# 14501번 퇴사
+# https://www.acmicpc.net/problem/14501
+import sys, os
+sys.stdin = open('{}/BOJ14501.txt'.format(os.path.dirname(os.path.realpath(__file__))))
+N=int(sys.stdin.readline())
