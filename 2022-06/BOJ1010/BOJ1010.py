fb31f0ce1c94a809f2682bf709a64eda4ef541e4
lzmgl <lzmgl5874@gmail.com>
2022-06-10 21:30:16 +0900

    20220610_4

--- 2022-06/BOJ1010/BOJ1010.py
+++ 2022-06/BOJ1010/BOJ1010.py
@@ -1,9 +1,8 @@
 # 1010번 다리 놓기
 # https://www.acmicpc.net/problem/1010
-from math import factorial
 import sys, os
-from itertools import combinations
 sys.stdin = open('{}/BOJ1010.txt'.format(os.path.dirname(os.path.realpath(__file__))))
+from math import factorial
 T=int(sys.stdin.readline())
 for _ in range(T):
     M, N = map(int, sys.stdin.readline().split())
