1b9fb493bbe41b06ba2199065c22d3a9e51891a9
lzmgl <lzmgl5874@gmail.com>
2022-06-10 22:18:02 +0900

    20220610_7

--- 2022-06/BOJ14501/BOJ14501.py
+++ 2022-06/BOJ14501/BOJ14501.py
@@ -3,7 +3,6 @@
 import sys, os
 sys.stdin = open('{}/BOJ14501.txt'.format(os.path.dirname(os.path.realpath(__file__))))
 N=int(sys.stdin.readline())
-<<<<<<< HEAD
 data = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 dp=[0]*(N+1)
 for i in range(N-1, -1, -1):
@@ -12,5 +11,3 @@ for i in range(N-1, -1, -1):
     else:
         dp[i]=max(dp[i+1], data[i][1]+dp[i+data[i][0]])
 print(dp[0])
-=======
->>>>>>> a3bdf077a1e78c4cd7a3634671fe25a644ab6239
