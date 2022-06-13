304e2166d29d907ef0281dacaeb065275caa80fd
lzmgl <lzmgl5874@gmail.com>
2022-06-14 03:55:28 +0900

    20220614_1

--- 2022-06/BOJ11057/BOJ11057.py
+++ 2022-06/BOJ11057/BOJ11057.py
@@ -2,4 +2,7 @@
 # https://www.acmicpc.net/problem/11057
 import sys, os
 sys.stdin = open('{}/BOJ11057.txt'.format(os.path.dirname(os.path.realpath(__file__))))
+from math import factorial
+mod=10007
 N=int(sys.stdin.readline())
+print((factorial(9+N)//(factorial(9)*factorial(N)))%mod)
