d0c74232f15d7fd842cc3ae4c6df839ccc4bd8c2
lzmgl <lzmgl5874@gmail.com>
2022-06-14 03:33:52 +0900

    20220614_0

---
+++ 2022-06/BOJ11057/BOJ11057.py
@@ -0,0 +1,5 @@
+# 11057번 오르막 수
+# https://www.acmicpc.net/problem/11057
+import sys, os
+sys.stdin = open('{}/BOJ11057.txt'.format(os.path.dirname(os.path.realpath(__file__))))
+N=int(sys.stdin.readline())
