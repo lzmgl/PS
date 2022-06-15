43ee859c8b67dbfd0e7a9d7daf8e1ed3c36024fe
lzmgl <lzmgl5874@gmail.com>
2022-06-16 03:51:04 +0900

    20220616_1

--- 2022-06/BOJ1075/BOJ1075.py
+++ 2022-06/BOJ1075/BOJ1075.py
@@ -3,3 +3,6 @@
 import sys, os
 sys.stdin = open('{}/BOJ1075.txt'.format(os.path.dirname(os.path.realpath(__file__))))
 N=int(sys.stdin.readline())
+F=int(sys.stdin.readline())
+tmp=N%F
+print(F-tmp)
