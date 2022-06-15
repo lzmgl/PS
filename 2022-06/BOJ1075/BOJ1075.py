9015dd30da95d30187c235abc1d7f912a5d73d46
lzmgl <lzmgl5874@gmail.com>
2022-06-16 03:38:59 +0900

    20220616_0

---
+++ 2022-06/BOJ1075/BOJ1075.py
@@ -0,0 +1,5 @@
+# 1075번 나누기
+# https://www.acmicpc.net/problem/1075
+import sys, os
+sys.stdin = open('{}/BOJ1075.txt'.format(os.path.dirname(os.path.realpath(__file__))))
+N=int(sys.stdin.readline())
