515f43c67f324d7bca6d6028528ac94d8b331a9c
lzmgl <lzmgl5874@gmail.com>
2022-06-16 04:16:25 +0900

    20220616_0

--- 2022-06/BOJ1075/BOJ1075.py
+++ 2022-06/BOJ1075/BOJ1075.py
@@ -4,5 +4,8 @@ import sys, os
 sys.stdin = open('{}/BOJ1075.txt'.format(os.path.dirname(os.path.realpath(__file__))))
 N=int(sys.stdin.readline())
 F=int(sys.stdin.readline())
-tmp=N%F
-print(F-tmp)
+S=N-(N%100)
+for i in range(S, S+99):
+    if not (i%F):
+        print(f'{i%100:0>2}')
+        exit()
