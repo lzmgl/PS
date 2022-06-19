7a6ac5ef42373abf3953d5b6efa97ebfadd82520
lzmgl <lzmgl5874@gmail.com>
2022-06-19 23:40:09 +0900

    20220619_2

---
+++ 2022-06/BOJ2420/BOJ2420.py
@@ -0,0 +1,6 @@
+# 2420번 사파리월드
+# https://www.acmicpc.net/problem/2420
+import sys, os
+sys.stdin = open('{}/BOJ2420.txt'.format(os.path.dirname(os.path.realpath(__file__))))
+a, b=map(int,sys.stdin.readline().split())
+print(abs(a)+abs(b))
