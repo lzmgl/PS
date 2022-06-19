e10ec46ec084a0fd5a7c78d627dbe1c268efcbc6
lzmgl <lzmgl5874@gmail.com>
2022-06-19 23:40:48 +0900

    20220619_3

--- 2022-06/BOJ2420/BOJ2420.py
+++ 2022-06/BOJ2420/BOJ2420.py
@@ -3,4 +3,4 @@
 import sys, os
 sys.stdin = open('{}/BOJ2420.txt'.format(os.path.dirname(os.path.realpath(__file__))))
 a, b=map(int,sys.stdin.readline().split())
-print(abs(a)+abs(b))
+print(abs(a-b))
