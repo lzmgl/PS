cd42ef9a77da5eb3db56cd16481e2ac933a70bb8
lzmgl <lzmgl5874@gmail.com>
2022-06-20 23:50:28 +0900

    20220620_0

---
+++ 2022-06/BOJ2738/BOJ2738.py
@@ -0,0 +1,5 @@
+# 2738번 행렬 덧셈
+# https://www.acmicpc.net/problem/2738
+import sys, os
+sys.stdin = open('{}/BOJ2738.txt'.format(os.path.dirname(os.path.realpath(__file__))))
+N=int(sys.stdin.readline())
