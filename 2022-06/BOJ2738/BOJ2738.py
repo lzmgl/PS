51fe2ce9fffdbf503cdfcf9c9fb2a89a09f0ea78
lzmgl <lzmgl5874@gmail.com>
2022-06-20 23:53:19 +0900

    20220620_1

--- 2022-06/BOJ2738/BOJ2738.py
+++ 2022-06/BOJ2738/BOJ2738.py
@@ -2,4 +2,15 @@
 # https://www.acmicpc.net/problem/2738
 import sys, os
 sys.stdin = open('{}/BOJ2738.txt'.format(os.path.dirname(os.path.realpath(__file__))))
-N=int(sys.stdin.readline())
+N, M=map(int,sys.stdin.readline().split())
+A=[list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+B=[list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+ans=[]
+for i in range(N):
+    tmp=[]
+    for j in range(M):
+        tmp.append(A[i][j]+B[i][j])
+    ans.append(tmp)
+
+for item in ans:
+    print(*item)
