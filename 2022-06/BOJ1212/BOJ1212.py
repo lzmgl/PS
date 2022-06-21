8bbc926a606f097f77dafde93ae5dab62d714b0e
lzmgl <lzmgl5874@gmail.com>
2022-06-21 23:10:25 +0900

    20220621_2

--- 2022-06/BOJ1212/BOJ1212.py
+++ 2022-06/BOJ1212/BOJ1212.py
@@ -2,12 +2,5 @@
 # https://www.acmicpc.net/problem/1212
 import sys, os
 sys.stdin = open('{}/BOJ1212.txt'.format(os.path.dirname(os.path.realpath(__file__))))
-N=int(sys.stdin.readline())
-cnt=sum=0
-while N>0:
-    tmp=N%10
-    tmp*=pow(8, cnt)
-    sum+=tmp
-    cnt+=1
-    N//=10
-print(bin(sum)[2:])
+N=(sys.stdin.readline().strip())
+print(bin(int(N, 8))[2:])
