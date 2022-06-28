6663816c930309aad56eb6a386d1c9fb7a925bb1
lzmgl <lzmgl5874@gmail.com>
2022-06-28 23:55:33 +0900

    20220628_1

---
+++ 2022-06/BOJ5337/BOJ5337.py
@@ -0,0 +1,7 @@
+# 5337번 웰컴
+# https://www.acmicpc.net/problem/5337
+import sys, os
+sys.stdin = open('{}/BOJ5337.txt'.format(os.path.dirname(os.path.realpath(__file__))))
+print('''.  .   .
+|  | _ | _. _ ._ _  _
+|/\|(/.|(_.(_)[ | )(/.''')
