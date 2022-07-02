fedbaa124727413fc0457d20e374e8118e0f4dcc
lzmgl <lzmgl5874@gmail.com>
2022-07-03 00:14:49 +0900

    20220703_0

---
+++ 2022-07/BOJ5338/BOJ5338.py
@@ -0,0 +1,9 @@
+# 5338번 마이크로소프트 로고
+# https://www.acmicpc.net/problem/5338
+import sys, os
+sys.stdin = open('{}/BOJ5338.txt'.format(os.path.dirname(os.path.realpath(__file__))))
+print('''       _.-;;-._
+'-..-'|   ||   |
+'-..-'|_.-;;-._|
+'-..-'|   ||   |
+'-..-'|_.-''-._|''')
