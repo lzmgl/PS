60f22098908db949e1b562624e7bf1038cf8ea6a
lzmgl <lzmgl5874@gmail.com>
2022-07-03 20:42:19 +0900

    20220703_1

---
+++ 2022-07/BOJ5339/BOJ5339.py
@@ -0,0 +1,14 @@
+# 5339번 콜센터
+# https://www.acmicpc.net/problem/5339
+import sys, os
+sys.stdin = open('{}/BOJ5339.txt'.format(os.path.dirname(os.path.realpath(__file__))))
+print('''     /~\\
+    ( oo|
+    _\=/_
+   /  _  \\
+  //|/.\|\\\\
+ ||  \ /  ||
+============
+|          |
+|          |
+|          |''')
