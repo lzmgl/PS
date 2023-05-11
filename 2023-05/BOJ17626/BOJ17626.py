e852c6f4f6431f7d47a3c7dd2996e7d11c16eaf6
lzmgl <lzmgl5874@gmail.com>
2023-05-11 17:54:29 +0900

    20230511_0

---
+++ 2023-05/BOJ17626/BOJ17626.py
@@ -0,0 +1,17 @@
+# 17626번 Four Squares
+# https://www.acmicpc.net/problem/17626
+import sys, os
+sys.stdin = open('{}/BOJ17626.txt'.format(os.path.dirname(os.path.realpath(__file__))))
+import math
+N=int(sys.stdin.readline())
+dp=[50001 for i in range(N+1)]
+
+dp[0]=0
+
+for i in range(1, N+1):
+    for j in range(1, i+1):
+        tmp=j**2
+        if tmp>i:
+            break
+        dp[i]=min(dp[i], dp[i-tmp]+1)
+print(dp[N])
