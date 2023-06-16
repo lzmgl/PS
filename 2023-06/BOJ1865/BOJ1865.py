7151c8a4d28df086cba3762d81da02fbd6fe24e1
lzmgl <lzmgl5874@gmail.com>
2023-06-16 17:49:34 +0900

    20230616_0

--- 2023-06/BOJ1865/BOJ1865.py
+++ 2023-06/BOJ1865/BOJ1865.py
@@ -2,17 +2,26 @@
 # https://www.acmicpc.net/problem/1865
 import sys, os
 sys.stdin = open('{}/BOJ1865.txt'.format(os.path.dirname(os.path.realpath(__file__))))
-from collections import defaultdict
-INF = int(1e15)
-TC=int(sys.stdin.readline())
-for _ in range(TC):
-    net=defaultdict(list)
-    N, M, W = map(int, sys.stdin.readline().split())
-    for j in range(M):
-        u, v, w = map(int, sys.stdin.readline().split())
-        net[u].append((v,w))
-        net[v].append((u,w))
-    for j in range(W):
-        S, E, T = map(int, sys.stdin.readline().split())
-        net[S].append((E,-T))
-    
+def sol():
+    from collections import defaultdict
+    TC=int(sys.stdin.readline())
+    for _ in range(TC):
+        net=defaultdict(list)
+        N, M, W = map(int, sys.stdin.readline().split())
+        for j in range(M):
+            u, v, w = map(int, sys.stdin.readline().split())
+            net[u].append((v,w))
+            net[v].append((u,w))
+        for j in range(W):
+            S, E, T = map(int, sys.stdin.readline().split())
+            net[S].append((E,-T))
+        
+    def bellman(graph, start):
+        INF = int(1e15)
+        distance = [INF]*(E+3)
+        distance[start] = 0
+        for v in graph:
+
+
+if __name__ == '__main__':
+    sol()
