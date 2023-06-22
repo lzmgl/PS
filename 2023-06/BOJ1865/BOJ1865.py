6ea5f18826013fb18c605bd0616b4bf8e4e6e598
lzmgl <lzmgl5874@gmail.com>
2023-06-22 19:45:47 +0900

    20230622_1

--- 2023-06/BOJ1865/BOJ1865.py
+++ 2023-06/BOJ1865/BOJ1865.py
@@ -4,7 +4,17 @@ import sys, os
 sys.stdin = open('{}/BOJ1865.txt'.format(os.path.dirname(os.path.realpath(__file__))))
 def sol():
     from collections import defaultdict
-    TC=int(sys.stdin.readline())
+    TC=int(sys.stdin.readline())        
+    def bellman(graph, start):
+        INF = int(1e15)
+        distance = [INF]*(E+3)
+        distance[start] = 0
+        for v in graph:
+            print(graph[v])
+            for item in graph[v]:
+                u=item[0]
+                w=item[1]
+                if v>
     for _ in range(TC):
         net=defaultdict(list)
         N, M, W = map(int, sys.stdin.readline().split())
@@ -15,12 +25,9 @@ def sol():
         for j in range(W):
             S, E, T = map(int, sys.stdin.readline().split())
             net[S].append((E,-T))
+        bellman(net, 0)
+
         
-    def bellman(graph, start):
-        INF = int(1e15)
-        distance = [INF]*(E+3)
-        distance[start] = 0
-        for v in graph:
 
 
 if __name__ == '__main__':
