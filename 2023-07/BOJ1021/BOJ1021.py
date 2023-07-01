e5a2b3f8d4d98a1aeb9a5bf11bf1cdc2ce8503b5
lzmgl <lzmgl5874@gmail.com>
2023-07-01 22:45:52 +0900

    20230701_2

--- 2023-07/BOJ1021/BOJ1021.py
+++ 2023-07/BOJ1021/BOJ1021.py
@@ -4,22 +4,22 @@ import sys, os
 sys.stdin = open('{}/BOJ1021.txt'.format(os.path.dirname(os.path.realpath(__file__))))
 from collections import deque
 N, M=map(int,sys.stdin.readline().split())
-data = list(map(int,sys.stdin.readline().split()))
+indexs = list(map(int,sys.stdin.readline().split()))
 i=0
 ans=0
 deq=deque(list(range(1,N+1)))
-for item in data:
+for idx in indexs:
     while 1:
-        if item==deq[0]:
+        if idx==deq[0]: #위치 = deque[0]
             deq.popleft()
             break
         else:
-            if deq.index(item)<len(deq)/2:
-                while deq[0]!=item:
+            if deq.index(idx)<len(deq)/2: # 왼쪽이면 rotate(-1)
+                while deq[0]!=idx:
                     deq.rotate(-1)
                     ans+=1
             else:
-                while deq[0]!=item:
+                while deq[0]!=idx: # 오른쪽이면 rotate(-1)
                     deq.rotate(1)
                     ans+=1
 
