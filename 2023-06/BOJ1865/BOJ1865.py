88aeeb6963f2efb4d4a05ce7e2858f7901313e52
lzmgl <lzmgl5874@gmail.com>
2023-07-04 19:51:29 +0900

    20230704_1

--- 2023-06/BOJ1865/BOJ1865.py
+++ 2023-06/BOJ1865/BOJ1865.py
@@ -2,31 +2,45 @@
 # https://www.acmicpc.net/problem/1865
 import sys, os
 sys.stdin = open('{}/BOJ1865.txt'.format(os.path.dirname(os.path.realpath(__file__))))
+def bellman_ford(graph, start):
+        distance, predecessor = dict(), dict()
+        for node in graph:
+            distance[node] = float('inf')
+            predecessor[node] = None
+        distance[start] = 0
+
+        for i in range(len(graph)-1):
+            for node in graph:
+                for neighbor in graph[node]:
+                    print(node)
+                    print(graph[node])
+                    print(neighbor)
+                    print(distance[neighbor])
+                    if distance[neighbor] > distance[node] + graph[node][neighbor]:
+                        distance[neighbor]=distance[node]+graph[node][neighbor]
+                        predecessor[neighbor] = node
+
+        for node in graph:
+            for neighbor in graph[node]:
+                if distance[neighbor] > distance[node] + graph[node][neighbor]:
+                    return -1
+        return distance, predecessor
 def sol():
     from collections import defaultdict
     TC=int(sys.stdin.readline())        
-    def bellman(graph, start):
-        INF = int(1e15)
-        distance = [INF]*(E+3)
-        distance[start] = 0
-        for v in graph:
-            print(v,graph[v])
-            for item in graph[v]:
-                # u=item[0]
-                # weight=item[1]
-                print(item)
-                # if v>
+    
+    
     for _ in range(TC):
         net=defaultdict(list)
         N, M, W = map(int, sys.stdin.readline().split())
         for j in range(M):
             u, v, w = map(int, sys.stdin.readline().split())
-            net[u].append({v:w})
-            net[v].append({u:w})
+            net[u].append([v,w])
+            net[v].append([u,w])
         for j in range(W):
             S, E, T = map(int, sys.stdin.readline().split())
-            net[S].append({E:-T})
-        bellman(net, 0)
+            net[S].append([E,-T])
+        print(bellman_ford(net, 0))
 
         
 
