a9e7e458b189ea1ad66e4d538703904f472b35b0
lzmgl <lzmgl5874@gmail.com>
2023-07-07 14:48:32 +0900

    20230707_3

--- 2023-07/BOJ16987/BOJ16987.py
+++ 2023-07/BOJ16987/BOJ16987.py
@@ -3,36 +3,40 @@
 import sys, os
 sys.stdin = open('{}/BOJ16987.txt'.format(os.path.dirname(os.path.realpath(__file__))))
 N=int(sys.stdin.readline())
-data=[]
+list_s=[]
+list_w=[]
+# data=[]
 for _ in range(N):
-    s, w = map(int, sys.stdin.readline().split())
-    data.append([s,w])
+    s, w = map(int, sys.stdin.readline().strip().split())
+    list_s.append(s)
+    list_w.append(w)
+    # data.append([s,w])
+    # 2차원 deepcopy를 slicing으로 안돼서 수정함.
 answer=0
 
 def egg(idx, eggs):
     global answer
-    if idx==N:
+    if idx==N: # 마지막계란이면 깨진거 계산(각 경우 다 계산해서 젤 많은거)
         cnt=0
-        print(eggs)
         for i in range(N):
-            if eggs[i][0]<1:
+            if eggs[i]<=0:
                 cnt+=1
         if cnt>answer:
             answer=cnt
         return 
-    if eggs[idx][0]>0:
+    if eggs[idx]>0: #든 계란이 안깨진거면
+        br=False #깰수있는 계란 체크
         for i in range(N):
-            br=False
-            if i!=idx and eggs[i][0]>0:
+            if i!=idx and eggs[i]>0:
                 br=True
-                tmp=(eggs[:])
-                tmp[i][0]-=eggs[idx][1]
-                tmp[idx][0]-=eggs[i][1]
-                egg(idx+1, tmp)
-        if not br:
-            egg(idx+1, eggs)
-    else:
-        egg(idx+1, eggs)
+                tmp=eggs[:] #깬 경우 복사해서 넘겨주기
+                tmp[i]-=list_w[idx]
+                tmp[idx]-=list_w[i]
+                egg(idx+1,tmp)
+        if not br: #깰수있는 계란이 없으니까 N으로 바로 넘겨
+            egg(N,eggs)
+    else: #깨진계란들면 넘기기
+        egg(idx+1,eggs)
 
-egg(0, data)
+egg(0, list_s)
 print(answer)
