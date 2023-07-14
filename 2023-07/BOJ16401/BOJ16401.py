0794ea163f75e9062ce1859d8521a1ee363f07e0
lzmgl5874 <lzmgl5874@gmail.com>
2023-07-14 16:22:14 +0900

    20230714_1

--- 2023-07/BOJ16401/BOJ16401.py
+++ 2023-07/BOJ16401/BOJ16401.py
@@ -2,4 +2,23 @@
 # https://www.acmicpc.net/problem/16401
 import sys, os
 sys.stdin = open('{}/BOJ16401.txt'.format(os.path.dirname(os.path.realpath(__file__))))
-N=int(sys.stdin.readline())
+def cut(data_list, h):
+    sumN=0
+    for item in data_list:
+        sumN+=item//h
+
+    return sumN
+
+def binarySearch(left, right, data, m):
+    while left<=right:
+        mid=(left+right)//2
+        if cut(data, mid)>=m:
+            left=mid+1
+        else:
+            right=mid-1
+    return right
+
+m,n=map(int,sys.stdin.readline().split())
+data=list(map(int, sys.stdin.readline().split()))
+data.sort()
+print(binarySearch(1, data[-1], data, m))
