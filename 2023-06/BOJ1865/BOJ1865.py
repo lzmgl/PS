d05d87ffe2938933eb9148e2053a51636cbe4cda
lzmgl <lzmgl5874@gmail.com>
2023-08-05 14:48:52 +0900

    20230805_2

--- 2023-06/BOJ1865/BOJ1865.py
+++ 2023-06/BOJ1865/BOJ1865.py
@@ -1,33 +1,8 @@
-# 1865번 웜홀 벨만포드 가중치음수
+# 1865번 웜홀 벨만포드 음수사이클
 # https://www.acmicpc.net/problem/1865
 import sys, os
 
 sys.stdin = open("{}/BOJ1865.txt".format(os.path.dirname(os.path.realpath(__file__))))
-
-
-def bellman_ford(graph, start):
-    distance, predecessor = dict(), dict()
-    for node in graph:
-        distance[node] = float("inf")
-        predecessor[node] = None
-    distance[start] = 0
-
-    for i in range(len(graph) - 1):
-        for node in graph:
-            for neighbor in graph[node]:
-                print(node)
-                print(graph[node])
-                print(neighbor)
-                print(distance[neighbor])
-                if distance[neighbor] > distance[node] + graph[node][neighbor]:
-                    distance[neighbor] = distance[node] + graph[node][neighbor]
-                    predecessor[neighbor] = node
-
-    for node in graph:
-        for neighbor in graph[node]:
-            if distance[neighbor] > distance[node] + graph[node][neighbor]:
-                return -1
-    return distance, predecessor
 
 
 def sol():
@@ -38,13 +13,26 @@ def sol():
     for _ in range(TC):
         net = defaultdict(list)
         N, M, W = map(int, sys.stdin.readline().split())
-        for j in range(M):
+        for _ in range(M):
             u, v, w = map(int, sys.stdin.readline().split())
             net[u].append([v, w])
             net[v].append([u, w])
-        for j in range(W):
+        for _ in range(W):
             S, E, T = map(int, sys.stdin.readline().split())
             net[S].append([E, -T])
+
+        def bellman_ford(graph, start):
+            distances = [10001] * (N + 1)
+            distances[start] = 0
+            for i in range(N):
+                for s in net:
+                    for d, w in net[s]:
+                        if distances[d] > distances[s] + w:
+                            if i == (N - 1):
+                                return "YES"
+                            distances[d] = distances[s] + w
+            return "NO"
+
         print(bellman_ford(net, 0))
 
 
