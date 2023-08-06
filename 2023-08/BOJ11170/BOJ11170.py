8ec959adcace00c211f74f4e7aa1d1d89f55158c
lzmgl <lzmgl5874@gmail.com>
2023-08-06 18:27:53 +0900

    20230806_5

--- 2023-08/BOJ11170/BOJ11170.py
+++ 2023-08/BOJ11170/BOJ11170.py
@@ -1,5 +1,13 @@
 # 11170번 0의 개수
 # https://www.acmicpc.net/problem/11170
 import sys, os
-sys.stdin = open('{}/BOJ11170.txt'.format(os.path.dirname(os.path.realpath(__file__))))
-N=int(sys.stdin.readline())
+
+sys.stdin = open("{}/BOJ11170.txt".format(os.path.dirname(os.path.realpath(__file__))))
+t = int(sys.stdin.readline())
+
+for _ in range(t):
+    n, m = map(int, sys.stdin.readline().split())
+    cnt = 0
+    for i in range(n, m + 1):
+        cnt += str(i).count("0")
+    print(cnt)
