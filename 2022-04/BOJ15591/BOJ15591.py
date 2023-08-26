20d4fec63223397ac8565bfc6447417756c6b52d
lzmgl <lzmgl5874@gmail.com>
2023-08-26 17:55:20 +0900

    20230826_2

--- 2022-04/BOJ15591/BOJ15591.py
+++ 2022-04/BOJ15591/BOJ15591.py
@@ -2,36 +2,41 @@
 # BFS 최소신장트리 MST graph defaultdict
 # https://www.acmicpc.net/problem/15591
 import sys, os
-sys.stdin = open('{}/BOJ15591.txt'.format(os.path.dirname(os.path.realpath(__file__))))
+
+sys.stdin = open("{}/BOJ15591.txt".format(os.path.dirname(os.path.realpath(__file__))))
 from collections import defaultdict, deque
-N, Q=map(int, sys.stdin.readline().split())
+
+N, Q = map(int, sys.stdin.readline().split())
 net = defaultdict(list)
-def moo(start_node, k,N , net):
-    queue=deque()
+
+
+def moo(start_node, k, N, net):
+    queue = deque()
     queue.append((start_node, 1000000000))
-    visited=[-1]*(N+1)
-    visited[start_node]=1
-    cnt=0
+    visited = [-1] * (N + 1)
+    visited[start_node] = 1
+    cnt = 0
     while queue:
         current, minN = queue.popleft()
         for next_node, dst in net[current]:
             if visited[next_node] == 1:
                 continue
-            if minN>dst:
+            if minN > dst:
                 queue.append((next_node, dst))
-                if dst>=k:
-                    cnt+=1
+                if dst >= k:
+                    cnt += 1
             else:
                 queue.append((next_node, minN))
-                if minN>=k:
-                    cnt+=1
-            visited[current]=1
-    return (cnt)
-for _ in range(N-1):
-    p, q, r = (map(int, sys.stdin.readline().split()))
+                if minN >= k:
+                    cnt += 1
+            visited[current] = 1
+    return cnt
+
+
+for _ in range(N - 1):
+    p, q, r = map(int, sys.stdin.readline().split())
     net[p].append((q, r))
     net[q].append((p, r))
 for _ in range(Q):
-    k, v = (map(int, sys.stdin.readline().split()))
+    k, v = map(int, sys.stdin.readline().split())
     print(moo(v, k, N, net))
-    
---
+++ 2023-08/BOJ2887/BOJ2887.py
@@ -0,0 +1,39 @@
+# 2887번 행성 터널
+# https://www.acmicpc.net/problem/2887
+import sys, os
+
+sys.stdin = open("{}/BOJ2887.txt".format(os.path.dirname(os.path.realpath(__file__))))
+N = int(sys.stdin.readline())
+parent = [i for i in range(N)]
+ans = 0
+data = []
+edges = []
+
+
+def find(parent, x):
+    if parent[x] != x:
+        parent[x] = find(parent, parent[x])
+    return parent[x]
+
+
+def union(parent, x, y):
+    x = find(parent, x)
+    y = find(parent, y)
+    parent[y] = x
+
+
+for i in range(N):
+    x, y, z = map(int, sys.stdin.readline().split())
+    data.append((x, y, z, i))
+for i in range(3):
+    data.sort(key=lambda x: x[i])
+    for j in range(1, N):
+        edges.append((abs(data[j - 1][i] - data[j][i]), data[j - 1][3], data[j][3]))
+edges.sort()
+
+for i in range(len(edges)):
+    if find(parent, edges[i][1]) != find(parent, edges[i][2]):
+        union(parent, edges[i][1], edges[i][2])
+        ans += edges[i][0]
+
+print(ans)
