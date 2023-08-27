36595f5adf85e6eba72c731e6364837506274e4d
lzmgl <lzmgl5874@gmail.com>
2023-08-27 22:37:20 +0900

    20230827_0

--- 2023-08/BOJ2887/BOJ2887.py
+++ 2023-08/BOJ2887/BOJ2887.py
@@ -25,12 +25,15 @@ def union(parent, x, y):
 for i in range(N):
     x, y, z = map(int, sys.stdin.readline().split())
     data.append((x, y, z, i))
+# edge 생성
 for i in range(3):
     data.sort(key=lambda x: x[i])
     for j in range(1, N):
         edges.append((abs(data[j - 1][i] - data[j][i]), data[j - 1][3], data[j][3]))
+# edge 정렬
 edges.sort()
 
+# 다른 set이면 합침
 for i in range(len(edges)):
     if find(parent, edges[i][1]) != find(parent, edges[i][2]):
         union(parent, edges[i][1], edges[i][2])
